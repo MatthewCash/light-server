83c9fbdf3fa4c739cdbc95c4f60783e4359cb2df
Matthew_Cash <matthew@matthew-cash.com>
2021-03-06 12:45:31 -0800

    Normalize hue

--- commands/setColor.ts
+++ commands/setColor.ts
@@ -1,11 +1,14 @@
-import { disableLightingEffect, runningEffect } from '../effects';
-import { bulbs, status } from '../main';
+import { disableLightingEffect } from '../effects';
+import { bulbs } from '../main';
 
 export const setColor = async (
     color: number,
     transitionSpeed = 1000
 ): Promise<void> => {
     disableLightingEffect();
+
+    while (color > 360) color -= 360;
+    while (color < 0) color += 360;
 
     await Promise.all(
         bulbs.map(bulb =>
