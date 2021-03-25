ca1b8acc7afd32214c0438c3f865daf14d51e529
Matthew_Cash <matthew@matthew-cash.com>
2021-03-25 16:28:22 -0700

    Use cached brightness and internal setBrightness method

--- commands/setBrightness.ts
+++ commands/setBrightness.ts
@@ -1,46 +1,15 @@
-import { bulbs } from '../main';
+import { bulbs, status } from '../main';
 
 export const setBrightness = async (
     brightness: number,
-    adjust: boolean = false
+    adjust = false
 ): Promise<void> => {
-    if (adjust) {
-        brightnessQueue.push(brightness);
-        if (!processingQueue) processBrightnessQueue();
-        return;
-    }
+    if (adjust) brightness += status.lighting.brightness;
 
     if (brightness < 0) brightness = 0;
     if (brightness > 100) brightness = 100;
 
     await Promise.all(
-        bulbs.map(bulb =>
-            bulb.setLighting({
-                brightness,
-                on_off: true
-            })
-        )
+        bulbs.map(bulb => bulb.setBrightness(brightness, 100, true))
     );
-};
-
-const brightnessQueue: number[] = [];
-let processingQueue = false;
-
-const processBrightnessQueue = async (): Promise<void> => {
-    processingQueue = true;
-    while (brightnessQueue.length > 0) {
-        const adjustment = brightnessQueue[0];
-
-        let {
-            brightness: currentBrightness
-        } = await bulbs[0].getLightingState();
-
-        let brightness = adjustment + currentBrightness;
-        if (brightness < 0) brightness = 0;
-        if (brightness > 100) brightness = 100;
-
-        await setBrightness(brightness);
-        brightnessQueue.shift();
-    }
-    processingQueue = false;
 };
